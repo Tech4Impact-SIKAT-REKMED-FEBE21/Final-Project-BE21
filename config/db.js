5662e422cd752aa04f3e6e86653c70b468cac843
KikiAndrianto <kikiandri25@gmail.com>
2022-11-29 19:00:16 +0700

    Membenarkan db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB_URL = ("mongodb://localhost:27017/rekmed-sikat");
+const DB_URL = ("mongodb://mongo:L20Zm8Gh4NPd0TkCd2hv@containers-us-west-117.railway.app:8064");
 
 const db = mongoose.connect(DB_URL);
 
